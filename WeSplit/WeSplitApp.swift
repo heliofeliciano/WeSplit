939a0166904cb4d6fb19a5c16a7f4c508abccdd7
Helio Feliciano <heliofeliciano@Helios-MacBook-Air.local>
2023-01-15 05:51:27 -0300

    Initital commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Helio Feliciano on 10/01/23.
+// 84 981587679
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

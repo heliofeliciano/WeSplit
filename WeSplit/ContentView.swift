3bbc65aa7a94babea10af1b2352a7f97cbe9af49
Helio Feliciano <heliofeliciano@gmail.com>
2023-02-20 19:52:04 -0300

    add conditional modifier in total amount

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -5,13 +5,19 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     let tipPercentages = [10, 15, 20, 25, 0]
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+    let currencyFormat: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currency?.identifier ?? "USD")
+    
+    var totalAmount: Double {
         let tipSelection = Double(tipPercentage)
         
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let amountPerPerson = totalAmount / peopleCount
         
         return amountPerPerson
     }
@@ -23,17 +29,17 @@ struct ContentView: View {
                 Section {
                     TextField("Amount",
                               value: $checkAmount,
-                              format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                              format: currencyFormat)
                     .keyboardType(.decimalPad)
                     .focused($amountIsFocused)
                 }
                 Section {
                     Picker("Tip precentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach( 0..<100 , id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.menu)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
@@ -45,7 +51,18 @@ struct ContentView: View {
                     }
                 }
                 Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text(totalPerPerson,
+                         format: currencyFormat)
+                } header: {
+                    Text("Amount per person")
+                }
+                Section {
+                    let textColor = tipPercentage == 0 ? Color.red : Color.black
+                    Text(totalAmount,
+                         format: currencyFormat)
+                    .foregroundColor(textColor)
+                } header: {
+                    Text("Total amount (Amount + Tip)")
                 }
             }
             .navigationTitle("WeSplit")
